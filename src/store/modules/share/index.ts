832d65970c56455594f81503b2bd4fdaf18d535d
zsbgnw12 <103713022+zsbgnw12@users.noreply.github.com>
2024-09-14 13:17:33 +0800

    Update index.ts

--- src/store/modules/share/index.ts
+++ src/store/modules/share/index.ts
@@ -1,10 +1,6 @@
 import { defineStore } from "pinia"
 
-interface newList {
-	title: string
-	link: string
-	snippet: string
-}
+
 
 export const useShareStore = defineStore("share-store", {
 	state: () => ({
